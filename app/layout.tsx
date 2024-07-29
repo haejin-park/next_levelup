15e5d9b26e71777c42bd6d2ee9740fd45ae77a8e
haejin <luckyhaejin1126@gmail.com>
2024-07-29 09:27:30 +0900

    메뉴 구성 및  레이아웃 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
-
-const inter = Inter({subsets:['latin']});
+import { GlobalNav } from "@/ui/global-nav";
 
 export const metadata: Metadata = {
   title: '레벨업 리액트 프로그래밍 with Next.js',
@@ -11,8 +9,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
-    <html lang="ko">
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <body className="bg-white overflow-y-scroll bg-[url('/grid.svg')] pb-36">
+        <GlobalNav />
+        <div className="lg:pl-72">
+          <div className="max-w-4xl px-2 pt-20 mx-auto space-y-8 lg:px-8">
+            <div className="p-px rounded-lg shadow-lg bg-vc-border-gradient shadow-black/20">
+              <div className="rounded-lg bg-white p-3.5 lg:p-6">
+                {children}
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
