890e704283dfc22287f7c19d7c4da39b6287f300
haejin <luckyhaejin1126@gmail.com>
2024-08-01 13:00:56 +0900

    라우트 그룹 ()로 감싸면 폴더를 묶어서 관리하되 폴더만들면 URL에는 영향을 미치지 않음

---
+++ app/route-groups/(marketing)/layout.tsx
@@ -0,0 +1,40 @@
+import { getTopCategories } from "@/data/category";
+import { Boundary } from "@/ui/boundary";
+import { ClickCounter } from "@/ui/click-counter";
+import { TabGroup } from "@/ui/tab-group";
+
+export default async function Layout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const categories = getTopCategories();
+
+    return (
+        <Boundary labels={['marketing layout']} color="violet" animateRerendering={false}>
+            <div className="space-y-9">
+                <div className="flex justify-between">
+                    <TabGroup 
+                        path="/route-groups"
+                        items={[
+                            {
+                                text: 'Home',
+                            },
+                            ...categories.map((x) => ({
+                                text: x.name,
+                                slug: x.slug,
+                            })),
+                            { text: '장바구니', slug: 'cart'},
+                            { text: '후기', slug: 'review'}
+                        ]}
+                    />
+                    <div className="self-start">
+                        <ClickCounter />
+                    </div>
+                </div>
+                <div>{children}</div>
+            </div>
+        </Boundary>
+        
+    )
+}
