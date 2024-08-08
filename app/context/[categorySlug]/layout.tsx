d42b4dc5430ff3919f07ba360fdfe288fb4b6ff3
haejin <luckyhaejin1126@gmail.com>
2024-08-08 17:58:39 +0900

    안쓰는 import 삭제

--- app/context/[categorySlug]/layout.tsx
+++ app/context/[categorySlug]/layout.tsx
@@ -2,7 +2,6 @@
 
 import { getCategories, getCategory } from "@/data/category";
 import { Boundary } from "@/ui/boundary";
-import { ClickCounter } from "@/ui/click-counter";
 import { TabGroup } from "@/ui/tab-group";
 import { Counter } from "../context-click-counter";
 
